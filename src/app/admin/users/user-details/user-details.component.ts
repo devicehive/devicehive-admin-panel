b2b05c36843e8b25c75edb0a2a0c687d3c287ce9
amedvedev <amedvyedyev@gmail.com>
2018-01-31 17:36:50 +0200

    Fixed typo

--- src/app/admin/users/user-details/user-details.component.ts
+++ src/app/admin/users/user-details/user-details.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'dh-user-details',
-  templateUrl: './user-detail.component.html',
+  templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.scss']
 })
 export class UserDetailsComponent implements OnInit {
