d3a85d34354bcbb23e8915808c3e39137e5bd97f
amedvedev <amedvyedyev@gmail.com>
2018-02-01 17:47:08 +0200

    Added placeholder dev environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  mainServiceURL: 'http://playground-dev.devicehive.com/api/rest',
+  authServiceURL: 'http://playground-dev.devicehive.com/auth/rest',
+  pluginServiceURL: 'http://playground-dev.devicehive.com/plugin/rest'
 };
