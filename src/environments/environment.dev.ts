112409bf861a28fc06fdb301d9d9250196fdea55
amedvedev <amedvyedyev@gmail.com>
2018-02-07 19:54:16 +0200

    Added dev environment

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  mainServiceURL: 'http://playground-dev.devicehive.com/api/rest',
+  authServiceURL: 'http://playground-dev.devicehive.com/auth/rest',
+  pluginServiceURL: 'http://playground-dev.devicehive.com/plugin/rest'
+};
